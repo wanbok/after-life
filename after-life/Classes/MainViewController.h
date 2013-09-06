0cd14f901b30794ac7361072cc6343837df34203
Wanbok Choi <choi@wanbok.com>
2013-09-07 00:32:39 +0900

    Adding basically code

---
+++ after-life/Classes/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  after-life
+//
+//  Created by Wan-bok Choi on 13. 9. 7..
+//  Copyright (c) 2013년 Wan-bok Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ after-life/Classes/MainViewController.m
@@ -0,0 +1,30 @@
+//
+//  MainViewController.m
+//  after-life
+//
+//  Created by Wan-bok Choi on 13. 9. 7..
+//  Copyright (c) 2013년 Wan-bok Choi. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+#pragma mark - UIViewController methods
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        // initializing code
+    }
+    return self;
+}
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+@end

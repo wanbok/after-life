10c69e839842b33981483457eb7eb65eb53ae95e
Wanbok Choi <choi@wanbok.com>
2013-09-06 23:56:26 +0900

    Init

---
+++ after-life/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  after-life
+//
+//  Created by Wan-bok Choi on 13. 9. 6..
+//  Copyright (c) 2013년 Wan-bok Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
